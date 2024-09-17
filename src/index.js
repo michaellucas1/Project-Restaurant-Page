085ae93514e0a8df5eba74e2794fbb93892637ad
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-09-16 22:26:12 -0400

    add a class that add and remove the event listener that changes the opacity of headers background color when scrolling down or up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./index.css";
+class MyComponent {
+    constructor(){
+        this.header = document.querySelector('.header');
+        this.addListener();
+    }
+    addListener(){
+        this.scrollOpacity = this.scrollOpacity.bind(this);
+        window.addEventListener('scroll',this.scrollOpacity);
+    }
+    removeAllListener(){
+        window.removeEventListener('scroll',this.scrollOpacity);
+    }
+    scrollOpacity(){
+        let scrollPos = window.scrollY;
+        if(scrollPos>300){
+            this.header.style.backgroundColor= `rgba(0,0,0,1)`;
+        }
+        else{
+            this.header.style.backgroundColor=`rgba(0,0,0,0.4)`;
+        }
+    }
+}
+new MyComponent();
