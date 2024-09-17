1f2e08535fbc3032aacbcd547703a4eab1d0e3ca
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-09-17 15:00:42 -0400

    add event listener to the nav buttons

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import "./index.css";
 class MyComponent {
     constructor(){
         this.header = document.querySelector('.header');
-        this.addListener();
+        this.addWindowListener();
+        this.addButtonListener();
     }
-    addListener(){
+    addWindowListener(){
         this.scrollOpacity = this.scrollOpacity.bind(this);
         window.addEventListener('scroll',this.scrollOpacity);
+    }
+    addButtonListener(){
+        this.home=document.querySelector("nav > button:nth-of-type(1");
+        this.menu=document.querySelector("nav > button:nth-of-type(2)");
+        this.contact=document.querySelector("nav > button:nth-of-type(3)");
+        this.about=document.querySelector("nav > button:nth-of-type(4)");
+        this.home.addEventListener("click",this.renderHomePage.bind(this));
+        this.menu.addEventListener("click",this.renderMenuPage.bind(this));
+        this.contact.addEventListener("click",this.renderContactPage.bind(this));
+        this.about.addEventListener("click",this.renderAboutPage.bind(this));
     }
     removeAllListener(){
         window.removeEventListener('scroll',this.scrollOpacity);
@@ -14,11 +25,23 @@ class MyComponent {
     scrollOpacity(){
         let scrollPos = window.scrollY;
         if(scrollPos>300){
-            this.header.style.backgroundColor= `rgba(0,0,0,1)`;
+            this.header.style.backgroundColor= `rgba(35, 32, 28,1)`;
         }
         else{
-            this.header.style.backgroundColor=`rgba(0,0,0,0.4)`;
+            this.header.style.backgroundColor=`rgba(35, 32, 28,0.3)`;
         }
+    }
+    renderHomePage(){
+        console.log("this is  the home page")
+    }
+    renderMenuPage(){
+        console.log("this is  the menu page")
+    }
+    renderContactPage(){
+        console.log("this is  the contact page")
+    }
+    renderAboutPage(){
+        console.log("this is  the about page")
     }
 }
 new MyComponent();
