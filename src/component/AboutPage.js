755b015c09e8715940a97987283fd110e5bfb347
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-09-22 21:17:25 -0400

    add js files that handle the page creation of About and Menu page

---
+++ src/component/AboutPage.js
@@ -0,0 +1,4 @@
+import Display from "./Display";
+import data from "./JSON/aboutPage.json"
+const aboutPage = new Display(data);
+export default aboutPage;
---
+++ src/component/MenuPage.js
@@ -0,0 +1,4 @@
+import Display from "./Display";
+import data from "./JSON/menuPage.json"
+const menuPage = new Display(data);
+export default menuPage;
